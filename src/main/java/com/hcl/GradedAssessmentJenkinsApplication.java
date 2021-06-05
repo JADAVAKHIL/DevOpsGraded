b39018026011a835dbae37a582b691a4ca76875c
JADAVAKHIL <jadavakhil7@gmail.com>
2021-06-05 19:14:17 +0530

    first commit by akhil

---
+++ src/main/java/com/hcl/GradedAssessmentJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssessmentJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssessmentJenkinsApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
